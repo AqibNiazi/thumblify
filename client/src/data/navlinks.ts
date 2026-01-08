a8fba9a3cb4bff8975657cd5e274d32307490e1d
AqibNiazi <aqibjaved5201@gmail.com>
2026-01-08 12:41:57 +0500

    Customizing Frontend application

---
+++ client/src/data/navlinks.ts
@@ -0,0 +1,9 @@
+import type { INavLink } from "../types";
+
+export const navlinks: INavLink[] = [
+  { name: "Home", href: "/" },
+  { name: "Generate", href: "/generate" },
+  { name: "My Generation", href: "/my-generation" },
+  { name: "Contact", href: "/contact" },
+  //   { name: "Login", href: "/login" },
+];
